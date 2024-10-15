91557b8c75161af574ea6501e8ba360aad13469c
Aleksandr <aleksandr.boldysev@stud.vilniustech.lt>
2024-10-15 23:17:24 +0300

    Implement time adjustment support, add mode indication

--- Firmware/bcd_clock.ino
+++ Firmware/bcd_clock.ino
@@ -5,11 +5,16 @@
 #define shcp_pin 6
 #define stcp_pin 5
 
+// Mode indication
+#define alarmLamp 7
+#define setLamp 8
+#define dateLamp 9
+
 // Keyboard
-#define snoozeButton 8 
-#define setButton 9 
-#define hoursButton 10
-#define minutesButton 11
+#define snoozeButton 10
+#define setButton 11
+#define hoursButton 12
+#define minutesButton 13
 
 RTC_DS3231 rtc;
 
@@ -37,21 +42,27 @@ unsigned long lastDebounceTimeMinutes = 0; // Timing for debounce
 // Debounce time
 const unsigned long debounceDelay = 50;
 
+enum : byte { REGULAR, SET_TIME, SET_DATE, SET_SLEEP_DELAY, SET_ALARM_TIME } setMode = REGULAR;
+
 void setup() {
   pinMode(ds_pin, OUTPUT);
   pinMode(stcp_pin, OUTPUT);
   pinMode(shcp_pin, OUTPUT);
-  pinMode(13, OUTPUT);
+
+  // Initialize mode indicator pins as output
+  pinMode(alarmLamp, OUTPUT);
+  pinMode(setLamp, OUTPUT);
+  pinMode(dateLamp, OUTPUT);
 
   // Initialize button pins as input
   pinMode(snoozeButton, INPUT); // Pin for Snooze Button
   pinMode(setButton, INPUT);     // Pin for Set Button
   pinMode(hoursButton, INPUT);   // Pin for Hours Button
   pinMode(minutesButton, INPUT); // Pin for Minutes Button
-  
+
   Serial.begin(9600);
   rtc.begin();
-  rtc.adjust(DateTime(2024,10,4,22,23,30)); // Настройка времени
+  rtc.adjust(DateTime(2024,10,15,22,23,30)); // Настройка времени
 }
 
 byte convertToBCD(int seconds) {
@@ -87,15 +98,66 @@ void handleSnoozeButtonPress() {
 }
 
 void handleSetButtonPress() {
-    Serial.println("Set button pressed.");
+    setMode = (setMode + 1) % 5;
+
+    if (setMode != REGULAR)
+      digitalWrite(setLamp, HIGH);
+    else
+      digitalWrite(setLamp, LOW);
 }
 
 void handleHoursButtonPress() {
-    Serial.println("Hours button pressed.");
+  // if not in set mode, display date
+  if (setMode == REGULAR) {
+    DateTime now = rtc.now();
+    digitalWrite(dateLamp, HIGH);
+    display(now.month(), 0, now.day());
+    delay(5000);
+    digitalWrite(dateLamp, LOW);
+  }
+  else if (setMode == SET_TIME)
+  {
+    DateTime current = rtc.now();
+
+    int hours = current.hour();
+    hours++;
+    if (hours == 24) hours = 0;
+
+    DateTime newDateTime(
+      current.year(), 
+      current.month(), 
+      current.day(),
+      hours,
+      current.minute(), 
+      current.second()
+    );
+
+    rtc.adjust(newDateTime);
+    display(newDateTime.hour(), newDateTime.minute(), 0);
+  }
 }
 
 void handleMinutesButtonPress() {
-    Serial.println("Minutes button pressed.");
+  if (setMode == SET_TIME)
+  {
+    DateTime current = rtc.now();
+
+    int minutes = current.minute();
+    minutes++;
+    if (minutes == 60) minutes = 0;
+
+    DateTime newDateTime(
+      current.year(), 
+      current.month(), 
+      current.day(),
+      current.hour(),
+      minutes, 
+      current.second()
+    );
+
+    rtc.adjust(newDateTime);
+    display(newDateTime.hour(), newDateTime.minute(), 0);
+  }
 }
 
 void handleButton(int buttonPin, int &buttonState, int &lastButtonState, unsigned long &lastDebounceTime, void (*buttonAction)()) {
@@ -128,9 +190,19 @@ void loop() {
   handleButton(hoursButton, hoursButtonState, lastHoursButtonState, lastDebounceTimeHours, handleHoursButtonPress);
   handleButton(minutesButton, minutesButtonState, lastMinutesButtonState, lastDebounceTimeMinutes, handleMinutesButtonPress);
 
-  // Если прошло 1000 миллисекунд
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    displayCurrentTime();  // Отображаем текущее время
+  if (setMode == REGULAR) {
+    // Refresh time once per second
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      displayCurrentTime();
+    }
+  }
+  else if (setMode == SET_TIME) {
+    // Refresh time once per second, but remove seconds
+     if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      DateTime now = rtc.now();
+      display(now.hour(), now.minute(), 0);
+    } 
   }
 }
